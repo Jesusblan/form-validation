e28f107d23374c14bd04ddab0b5dd30e4888df69
JesusBlan <jezublanco@gmail.com>
2025-02-13 21:13:44 +0100

    added validation criteria to mail input and passwords inputs

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import { greeting } from "./home.js";
 
 
 const email = document.querySelector("#emailInput");
-const country = document.querySelector("#countryInput");
-const postalCode = document.querySelector("#postalInput");
-const password = document.querySelector("#passwordInput");
-const passwordConfirm = document.querySelector("#passwordConfInput");
 
-email.addEventListener("input", (e) =>{
+email.addEventListener("input", () =>{
     if (email.validity.tymeMismatch) {
         email.setCustomValidity("This is not an email adress!");
     }
@@ -17,3 +13,42 @@ email.addEventListener("input", (e) =>{
     }
 })
 
+
+const password = document.querySelector("#passwordInput");
+
+
+password.addEventListener("input", () =>{
+    if (password.validity.tooShort){
+        password.setCustomValidity("Password must contain 8 char or more");
+    }
+    else if (password.validity.tooLong){
+        password.setCustomValidity("Password must contain 15 char. or less");
+    }
+    
+    else {
+        password.setCustomValidity("");
+    }
+ })
+
+
+const passwordConfirm = document.querySelector("#passwordConfInput");
+
+passwordConfirm.addEventListener("input", () =>{
+    if (passwordConfirm.validity.tooShort){
+        passwordConfirm.setCustomValidity("Password must contain 8 char or more");
+    }
+    else if (passwordConfirm.validity.tooLong){
+        passwordConfirm.setCustomValidity("Password must contain 15 char. or less");
+    }
+    else {
+        passwordConfirm.setCustomValidity("");
+    }
+ })
+
+
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+})
