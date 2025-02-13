e98bb44c8e70f4522305ec54fdfbc642ffe988df
JesusBlan <jezublanco@gmail.com>
2025-02-13 14:02:54 +0100

    aded a form validation in the email input with js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import "./styles.css";
 import { greeting } from "./home.js";
 
-console.log(greeting);
+
+const email = document.querySelector("#emailInput");
+const country = document.querySelector("#countryInput");
+const postalCode = document.querySelector("#postalInput");
+const password = document.querySelector("#passwordInput");
+const passwordConfirm = document.querySelector("#passwordConfInput");
+
+email.addEventListener("input", (e) =>{
+    if (email.validity.tymeMismatch) {
+        email.setCustomValidity("This is not an email adress!");
+    }
+    else {
+        email.setCustomValidity("");
+    }
+})
+
